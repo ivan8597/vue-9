4339138acec9abad21f4f94bf787b0831c32e98f
Иван Меркулов <ivanmerkulov@Air-Ivan.Dlink>
2025-02-05 18:15:59 +0300

    первый коммит

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,9 @@
 /** @type {import('tailwindcss').Config} */
-<<<<<<< HEAD
 module.exports = {
   content: [
-    "./pages/**/*.{vue,js,ts,jsx,tsx}",
     "./components/**/*.{vue,js,ts,jsx,tsx}",
+    "./src/**/*.{vue,js,ts,jsx,tsx}", 
   ],
-=======
-export default {
-  // purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
-  content: ["./src/**/*.{vue,js,ts,jsx,tsx}"],
->>>>>>> 1110c30 (vue)
   theme: {
     extend: {},
   },
